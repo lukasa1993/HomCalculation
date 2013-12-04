7a35a9b833a05351519f9e5956ffe689a13cff38
lukasa1993 <luka@picktek.com>
2013-12-04 10:39:49 +0400

    Initial Commit

---
+++ HomCalculation/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HomCalculation
+//
+//  Created by Luka Dodelia on 12/4/13.
+//  Copyright (c) 2013 Luka Dodelia. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
