81e68f1db7e5ee23fb4c50529b084743b537055f
lukasa1993 <luka@picktek.com>
2013-12-16 14:05:26 +0400

    working version

--- HomCalculation/main.c
+++ HomCalculation/main.c
@@ -15,7 +15,7 @@
 
 int main(int argc, const char * argv[])
 {
-    /*
+
     char* firstComplex  = (char*) malloc(MAX_STRING_LEN * sizeof(char));
     char* secondComplex = (char*) malloc(MAX_STRING_LEN * sizeof(char));
     
@@ -27,30 +27,13 @@ int main(int argc, const char * argv[])
     printf("Second Complex Please:\t");
     scanf("%s", secondComplex);
     
-    */
     printf("\n ---- As We Saw it ---- \n");
     
-    Complex* complex1 = literalToComplex("[[1,2,3], [2,3], [1,3]]");
-    Complex* complex2 = literalToComplex("[[1,2],[2,3]]");
+    Complex* complex1 = literalToComplex(firstComplex);
+    Complex* complex2 = literalToComplex(secondComplex);
     printf("%s\n", complexToLiteral(complex1, true));
     printf("%s\n", complexToLiteral(complex2, true));
     
     printf("\nThe End!\n");
     return 0;
 }
-
-
-/*
- 
- int vertexCount   = 10;
- Simplex*  simplex = Init_Simplex(vertexCount, true);
- Simplex** subSimp = AllSubSimplexses(simplex);
- for (int i = 0; i < 1 << vertexCount; ++i) {
- Simplex* simp = subSimp[i];
- for (int j = 0; j < simp->verticesCount; ++j) {
- printf(" %i ", simp->simplexRel[j]);
- }
- printf("\n");
- }
- 
- */
