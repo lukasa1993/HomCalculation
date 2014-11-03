96a10ece791ba3042358e9b5440f06504b129b18
lukasa1993 <luka@picktek.com>
2014-11-03 18:44:07 +0400

    trying once again

--- HomCalculation/main.c
+++ HomCalculation/main.c
@@ -193,11 +193,11 @@ int main(int argc, const char *argv[]) {
     char* B = "[[1,2,6],[2,3,4],[4,5,6],[2,4,6]]";
     char* C = "[[1,2,4],[2,3,5],[3,1,6],[1,4,6],[4,2,5],[4,5,6],[3,6,5]]";
 
-    Calculate_Hom(literalToComplex(A), literalToComplex(A));
-    Calculate_Hom(literalToComplex(A), literalToComplex(B));
-
-    Calculate_Hom(literalToComplex(B), literalToComplex(A));
-    Calculate_Hom(literalToComplex(B), literalToComplex(B));
+//    Calculate_Hom(literalToComplex(A), literalToComplex(A));
+//    Calculate_Hom(literalToComplex(A), literalToComplex(B));
+//
+//    Calculate_Hom(literalToComplex(B), literalToComplex(A));
+//    Calculate_Hom(literalToComplex(B), literalToComplex(B));
 
 
     // ------ can't calcualate? --------------
